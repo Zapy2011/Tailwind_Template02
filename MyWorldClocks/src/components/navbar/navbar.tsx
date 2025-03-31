f58125ab4f7b3b7e949e05b831577658dfe17906
Zapy2011 <aiden.sok@gmail.com>
2025-03-30 19:42:21 -0500

    add navbar using stripe navbar template

--- MyWorldClocks/src/components/navbar/navbar.tsx
+++ MyWorldClocks/src/components/navbar/navbar.tsx
@@ -1,14 +1,16 @@
-import { Link } from '@builder.io/qwik-city';
+import { useSignal } from '@builder.io/qwik';
 
 export const Navbar = () => {
+    
     return (
         <header class="bg-white">
-            <nav class="flex justify-between items-center w-[92%]  mx-auto">
+            <nav class="flex justify-between items-center w-[92%] mx-auto">
                 <div>
                     <img class="w-16 cursor-pointer" src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png" alt="..." />
                 </div>
                 <div
-                    class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+                    
+                    class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
                     <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                         <li>
                             <a class="hover:text-gray-500" href="#">Products</a>
@@ -27,10 +29,7 @@ export const Navbar = () => {
                         </li>
                     </ul>
                 </div>
-                <div class="flex items-center gap-6">
-                    <button class="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Sign in</button>
-                    <ion-icon onclick="onToggleMenu(this)" name="menu" class="text-3xl cursor-pointer md:hidden"></ion-icon>
-                </div>
+                
             </nav>
         </header>
     );
