45e593593e40743ec7388ac79fc2c42d3899f616
Sam <sambo.sok@gmail.com>
2025-05-03 22:09:43 -0500

    Refactor code structure for improved readability and maintainability

--- MyWorldClocks/src/components/router-head/router-head.tsx
+++ MyWorldClocks/src/components/router-head/router-head.tsx
@@ -10,11 +10,9 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>{head.title}</title>
-
-      <link rel="canonical" href={loc.url.href} />
+      <title>{head.title}</title>      <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
